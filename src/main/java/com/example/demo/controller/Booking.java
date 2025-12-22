f4b9c7000a3ad92e522426fd52d8c58933fd8f55
bala-amypo <bala@amypo.in>
2025-12-22 04:46:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/Booking.java
@@ -0,0 +1,37 @@
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Facility facility;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(nullable = false)
+    private String status = "CONFIRMED";
+
+    @OneToMany(mappedBy = "booking")
+    private List<BookingLog> logs;
+}
---
+++ src/main/java/com/example/demo/controller/Facility.java
@@ -0,0 +1,29 @@
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "facilities", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Facility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private String openTime;
+
+    @Column(nullable = false)
+    private String closeTime;
+}
