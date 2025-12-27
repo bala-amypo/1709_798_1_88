14c6dd6dc7f8ff4c682f9ec4a8a6d1562b2307d7
bala-amypo <bala@amypo.in>
2025-12-27 04:24:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ApartmentUnitController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.ApartmentUnit;
+import com.example.demo.service.ApartmentUnitService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/units")
+public class ApartmentUnitController {
+
+    private final ApartmentUnitService apartmentUnitService;
+
+    public ApartmentUnitController(ApartmentUnitService apartmentUnitService) {
+        this.apartmentUnitService = apartmentUnitService;
+    }
+
+    @PostMapping("/assign/{userId}")
+    public ResponseEntity<ApartmentUnit> assignUnit(
+            @PathVariable Long userId,
+            @RequestBody ApartmentUnit unit) {
+
+        return ResponseEntity.ok(
+                apartmentUnitService.assignUnitToUser(userId, unit)
+        );
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<ApartmentUnit> getUnitByUser(
+            @PathVariable Long userId) {
+
+        return ResponseEntity.ok(
+                apartmentUnitService.getUnitByUser(userId)
+        );
+    }
+}
