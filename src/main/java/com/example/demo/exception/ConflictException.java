a559043f1200f4d2f6e119314256d1b92f526eb7
bala-amypo <bala@amypo.in>
2025-12-27 05:23:46 +0000

    User commit

--- src/main/java/com/example/demo/exception/ConflictException.java
+++ src/main/java/com/example/demo/exception/ConflictException.java
@@ -1,8 +1,7 @@
 package com.example.demo.exception;
 
-public class DuplicateResourceException extends RuntimeException {
-
-    public DuplicateResourceException(String message) {
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
         super(message);
     }
 }
