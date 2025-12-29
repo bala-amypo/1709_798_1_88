8a15a7105f87719c6fba1346d7b08179c14211a7
bala-amypo <bala@amypo.in>
2025-12-29 06:43:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/FacilityRepository.java
+++ src/main/java/com/example/demo/repository/FacilityRepository.java
@@ -2,7 +2,5 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FacilityRepository extends JpaRepository<Facility, Long> {}
